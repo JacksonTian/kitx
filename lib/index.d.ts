0ae4d1cb79c6010f080557a7f4bd09e081ee3056
Jackson Tian <shyvo1987@gmail.com>
2020-02-14 14:46:56 +0800

    Bump 2.1.0

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -1,11 +1,11 @@
 import { Readable } from 'stream';
 
 interface Hash {
-    (data: string|Buffer, encoding?: string|undefined): string|Buffer;
+    (data: string | Buffer, encoding?: string | undefined): string | Buffer;
 }
 
 interface Hmac {
-    (data: string|Buffer, key: string|Buffer, encoding?: string|undefined): string|Buffer;
+    (data: string | Buffer, key: string | Buffer, encoding?: string | undefined): string | Buffer;
 }
 
 export function createHash(algorithm: string): Hash;
@@ -26,7 +26,7 @@ export function makeHasher(algorithm: string): Hash;
 
 export function makeNonce(): string;
 
-export function md5(data: string|Buffer, encoding?: string|undefined): string;
+export function md5(data: string | Buffer, encoding?: string | undefined): string;
 
 export function pad2(num: number): string;
 
@@ -34,7 +34,7 @@ export function pad3(num: number): string;
 
 export function random(min: number, max: number): number;
 
-export function sha1(data: string|Buffer, key: string, encoding?: string|undefined): string|Buffer;
+export function sha1(data: string | Buffer, key: string, encoding?: string | undefined): string | Buffer;
 
 export function sleep(ms: number): Promise<void>;
 
